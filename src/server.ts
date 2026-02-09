47da199c8f6254dc942b0b45c5a1ed852746da9c
Marcelle <marcellealves.dev@gmail.com>
2026-02-08 21:50:51 -0300

    chore: prepare for deployment

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,8 @@ import { env } from './env/index'
 
 app
   .listen({
-    port: env.PORT
+    port: env.PORT,
+    host: 'RENDER' in process.env ? '0.0.0.0' : 'localhost'
   })
   .then(() => {
     console.log(`HTTP server running`)
